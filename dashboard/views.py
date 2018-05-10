c0dd0b6be51c54777f7c505410526b5324e6396a
Lucas <lleal_melo@hotmail.com>
2018-05-10 01:23:45 -0300

    Criei a página de Dashboard Precisamos mudar algumas coisas na pagina de protocolos: Colocar cor dinâmica na expectativa das contas Melhorar a visualização da soma de premio Colocar separação de milhar

---
+++ dashboard/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from home.models import Protocolos
+from home.forms import PipelineComercial
+from django.http import HttpResponseRedirect
+
+
+def dashboard(request):
+    return render(request, 'dashboard/dashboard_base.html', {'dashboard': dashboard})
