8d63da6c487b7a2a841c8384f8837c577728f57c
Lucas <lleal_melo@hotmail.com>
2018-06-26 00:19:58 -0300

    Definição de logo e inicio da transformação do layout

--- home/forms.py
+++ home/forms.py
@@ -8,21 +8,21 @@ class PipelineComercial(forms.ModelForm):
         attrs={
             'class': 'form-control',
             'id': 'regional',
-            'style': 'background-color: #ebebeb; border:1px solid #444444'
+            'style': 'background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     filial = forms.ModelChoiceField(queryset=Filial.objects.all(), widget=forms.Select(
         attrs={
             'class': 'form-control',
             'id': 'filial',
-            'style': 'background-color: #ebebeb; border:1px solid #444444'
+            'style': 'background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     comercial = forms.ModelChoiceField(queryset=Comercial.objects.all(), widget=forms.Select(
         attrs={
             'class': 'form-control',
             'id': 'comercial',
-            'style': 'background-color: #ebebeb; border:1px solid #444444'
+            'style': 'background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     corretor = forms.CharField(max_length=200, widget=forms.TextInput(
@@ -31,7 +31,7 @@ class PipelineComercial(forms.ModelForm):
             'id': 'corretor',
             'placeholder': 'Escreva aqui o nome do corretor',
             'type': 'text',
-            'style': 'background-color: #ebebeb; border:1px solid #444444'
+            'style': 'background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     premio = forms.CharField(max_length=200, widget=forms.TextInput(
@@ -40,7 +40,7 @@ class PipelineComercial(forms.ModelForm):
             'id': 'premio',
             'placeholder': 'Insira o valor mensal da conta',
             'type': 'number',
-            'style': '-webkit-appearance: none;-moz-appearance: textfield; background-color: #ebebeb; border:1px solid #444444',
+            'style': '-webkit-appearance: none;-moz-appearance: textfield; background-color: #f2f2f2; border:1px solid #444444',
 
         }
     ))
@@ -49,7 +49,7 @@ class PipelineComercial(forms.ModelForm):
             'class': 'form-control',
             'placeholder': 'Esolha o produto',
             'id': 'produto',
-            'style': 'background-color: #ebebeb; border:1px solid #444444'
+            'style': 'background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     cliente = forms.CharField(max_length=200, widget=forms.TextInput(
@@ -58,7 +58,7 @@ class PipelineComercial(forms.ModelForm):
             'id': 'cliente',
             'placeholder': 'Escreva aqui o nome da empresa cliente',
             'type': 'text',
-            'style': 'background-color: #ebebeb; border:1px solid #444444'
+            'style': 'background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     subscritor = forms.CharField(max_length=200, widget=forms.TextInput(
@@ -66,7 +66,7 @@ class PipelineComercial(forms.ModelForm):
             'class': 'form-control',
             'id': 'subscritor',
             'type': 'text',
-            'style': 'background-color: #ebebeb; border:1px solid #444444'
+            'style': 'background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     recebimento = forms.DateField(input_formats=['%d/%m/%Y'], widget=forms.TextInput(
@@ -74,7 +74,7 @@ class PipelineComercial(forms.ModelForm):
             'class': 'form-control',
             'id': 'recebimento',
             'type': 'text',
-            'style': 'background-color: #ebebeb; border:1px solid #444444'
+            'style': 'background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     fechamento = forms.DateField(input_formats=['%d/%m/%Y'], widget=forms.TextInput(
@@ -83,7 +83,7 @@ class PipelineComercial(forms.ModelForm):
             'id': 'fechamento',
             'placeholder': 'Data prevista para fechamento',
             'type': 'text',
-            'style': 'background-color: #ebebeb; border:1px solid #444444'
+            'style': 'background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     vencimento = forms.DateField(input_formats=['%d/%m/%Y'], widget=forms.TextInput(
@@ -92,41 +92,42 @@ class PipelineComercial(forms.ModelForm):
             'id': 'vencimento',
             'placeholder': 'Data de vencimento da apólice',
             'type': 'text',
-            'style': 'background-color: #ebebeb; border:1px solid #444444'
+            'style': 'background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     tipo_de_seguro = forms.ModelChoiceField(queryset=TipodeSeguro.objects.all(), widget=forms.Select(
         attrs={
             'class': 'form-control',
             'id': 'tipo_de_seguro',
-            'style': 'background-color: #ebebeb; border:1px solid #444444'
+            'style': 'background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     congenere = forms.ModelChoiceField(required=False, queryset=Congenere.objects.all(), widget=forms.Select(
         attrs={
             'class': 'form-control',
             'id': 'congenere',
-            'style': 'background-color: #ebebeb; border:1px solid #444444'
+            'style': 'background-color: #f2f2f2; border:1px solid #0dbf7e'
         }
     ))
     expectativa = forms.ModelChoiceField(queryset=Expectativa.objects.all(), widget=forms.Select(
         attrs={
             'class': 'form-control',
             'id': 'expectativa',
-            'style': 'background-color: #ebebeb; border:1px solid #444444'
+            'style': 'background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     status = forms.ModelChoiceField(queryset=Status.objects.all(), widget=forms.Select(
         attrs={
             'class': 'form-control',
             'id': 'status',
-            'style': 'background-color: #ebebeb; border:1px solid #444444'
+            'style': 'background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     motivo_perda = forms.ModelChoiceField(required=False, queryset=MotivoPerda.objects.all(), widget=forms.Select(
         attrs={
             'class': 'form-control',
-            'id': 'motivo_perda'
+            'id': 'motivo_perda',
+            'style': 'background-color: #f2f2f2; border:1px solid #0dbf7e'
         }
     ))
     comentario_perda = forms.CharField(required=False,max_length=200, widget=forms.Textarea(
@@ -135,7 +136,7 @@ class PipelineComercial(forms.ModelForm):
             'id': 'comentario_perda',
             'rows': '1',
             'placeholder': 'Comente brevemente o motivo da perda',
-            'style': 'resize: none'
+            'style': 'resize: none; background-color: #f2f2f2; border:1px solid #0dbf7e'
         }
     ))
     historico_1 = forms.CharField(required=False,max_length=3000, widget=forms.Textarea(
@@ -144,7 +145,7 @@ class PipelineComercial(forms.ModelForm):
             'id': 'historico_1',
             'rows': '2',
             'placeholder': 'Historico 1',
-            'style': 'resize: none; background-color: #ebebeb; border:1px solid #444444'
+            'style': 'resize: none; background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     historico_2 = forms.CharField(required=False,max_length=3000, widget=forms.Textarea(
@@ -153,7 +154,8 @@ class PipelineComercial(forms.ModelForm):
             'id': 'historico_2',
             'rows': '2',
             'placeholder': 'Historico 2',
-            'style': 'resize: none'
+            'style': 'resize: none; background-color: #f2f2f2; border:1px solid #444444'
+
         }
     ))
     historico_3 = forms.CharField(required=False,max_length=3000, widget=forms.Textarea(
@@ -162,7 +164,7 @@ class PipelineComercial(forms.ModelForm):
             'id': 'historico_3',
             'rows': '2',
             'placeholder': 'Historico 3',
-            'style': 'resize: none'
+            'style': 'resize: none;  background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     historico_4 = forms.CharField(required=False,max_length=3000, widget=forms.Textarea(
@@ -171,7 +173,7 @@ class PipelineComercial(forms.ModelForm):
             'id': 'historico_4',
             'rows': '2',
             'placeholder': 'Historico 4',
-            'style': 'resize: none'
+            'style': 'resize: none;  background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     historico_5 = forms.CharField(required=False,max_length=3000, widget=forms.Textarea(
@@ -180,7 +182,7 @@ class PipelineComercial(forms.ModelForm):
             'id': 'historico_5',
             'rows': '2',
             'placeholder': 'Historico 5',
-            'style': 'resize: none'
+            'style': 'resize: none;  background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     historico_6 = forms.CharField(required=False,max_length=3000, widget=forms.Textarea(
@@ -189,7 +191,7 @@ class PipelineComercial(forms.ModelForm):
             'id': 'historico_6',
             'rows': '2',
             'placeholder': 'Historico 6',
-            'style': 'resize: none'
+            'style': 'resize: none;  background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     historico_7 = forms.CharField(required=False,max_length=3000, widget=forms.Textarea(
@@ -198,7 +200,7 @@ class PipelineComercial(forms.ModelForm):
             'id': 'historico_7',
             'rows': '2',
             'placeholder': 'Historico 7',
-            'style': 'resize: none'
+            'style': 'resize: none;  background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     historico_8 = forms.CharField(required=False,max_length=3000, widget=forms.Textarea(
@@ -207,7 +209,7 @@ class PipelineComercial(forms.ModelForm):
             'id': 'historico_8',
             'rows': '2',
             'placeholder': 'Historico 8',
-            'style': 'resize: none'
+            'style': 'resize: none;  background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     historico_9 = forms.CharField(required=False,max_length=3000, widget=forms.Textarea(
@@ -216,7 +218,7 @@ class PipelineComercial(forms.ModelForm):
             'id': 'historico_9',
             'rows': '2',
             'placeholder': 'Historico 9',
-            'style': 'resize: none'
+            'style': 'resize: none;  background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     historico_10 = forms.CharField(required=False,max_length=3000, widget=forms.Textarea(
@@ -225,7 +227,7 @@ class PipelineComercial(forms.ModelForm):
             'id': 'historico_10',
             'rows': '2',
             'placeholder': 'Historico 10',
-            'style': 'resize: none'
+            'style': 'resize: none;  background-color: #f2f2f2; border:1px solid #444444'
         }
     ))
     class Meta:
